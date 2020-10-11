730fd31ba756c456e11476286ec7a1a1ecf727d9
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-11 11:50:04 -0400

    Creates Camera Vision files
    This makes a file which does nothing as of yet, but sets it up to begin creating the Pipeline which allows the analysis of ring height.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameravision/HeightDetector.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.cameravision;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class HeightDetector {
+    /**
+     * This connects to the phone's camera to capture an image of the rings.
+     * <p>
+     * Currently, this uses the {@link OpenCvInternalCamera} class to temporally get
+     * access to advanced features. However, when we switch to a web camera, this will have to be
+     * refactored to use the webcam.
+     */
+    private OpenCvInternalCamera camera;
+
+    public HeightDetector(HardwareMap hardwareMap) {
+        init(hardwareMap);
+    }
+
+    private void init(HardwareMap hardwareMap) {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId",
+                "id",
+                hardwareMap.appContext.getPackageName()
+        );
+        camera = OpenCvCameraFactory.getInstance().createInternalCamera(
+                OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId
+        );
+
+        // Sets up the Camera to use the pipeline which detects the height of the rings
+        camera.setPipeline(new RingHeightPipeline());
+
+        // Starts Streaming the Camera Contents to the phone
+        camera.openCameraDeviceAsync(() ->
+                camera.startStreaming(640 /*320*/, 480 /*240*/, OpenCvCameraRotation.UPRIGHT)
+        );
+    }
+
+    static class RingHeightPipeline extends OpenCvPipeline {
+        @Override
+        public Mat processFrame(Mat input) {
+            return null;
+        }
+    }
+}
