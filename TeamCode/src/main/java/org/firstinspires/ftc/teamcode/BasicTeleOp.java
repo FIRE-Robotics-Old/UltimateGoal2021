0b808cdcf8b9f2a3296363a9b5ac2d2da22a76d9
Emily Moyal <72623454+EmlyUwUMoyal@users.noreply.github.com>
2020-10-16 03:13:09 +0300

    Adds basic teleop file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+(name="Basic TeleOp  ", group="Linear Opmode")
+
+public class BasicTeleOp extends LinearOpMode {
+
+    public DcMotor fL;
+    public DcMotor fR;
+    public DcMotor bL;
+    public DcMotor bR;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        fL = hardwareMap.dcMotor.get("frontLeftMotor");
+        fR = hardwareMap.dcMotor.get("frontRightMotor");
+        bL = hardwareMap.dcMotor.get("backLeftMotor");
+        bR = hardwareMap.dcMotor.get("backRightMotor");
+
+        fL.setDirection(DcMotor.Direction.FORWARD);
+        fR.setDirection(DcMotor.Direction.FORWARD);
+        bL.setDirection(DcMotor.Direction.REVERSE);
+        bR.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double drive = gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double twist = gamepad1.right_stick_x;
+
+            double [] speeds = {
+                    (drive + strafe + twist),
+                    (drive - strafe - twist),
+                    (drive - strafe + twist),
+                    (drive + strafe - twist)
+            };
+
+            double max = Math.abs(speeds [0]);
+            for (int i = 0; i < speeds.length; i++) {
+                if (max < Math.abs(speeds[i])) max = Math.abs(speeds[i]);
+            }
+
+            if (max > 1) {
+                for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
+
+            }
+
+            fL.setPower(speeds[0]);
+            fR.setPower(speeds[1]);
+            bL.setPower(speeds[2]);
+            bR.setPower(speeds[3]);
+
+        }
+
+    }
+}
