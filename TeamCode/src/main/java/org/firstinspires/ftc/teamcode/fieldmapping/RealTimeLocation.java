cea11039023224b1d69af52be2de5d5f4c83fb30
Marvin4213 <52053423+marvin4213@users.noreply.github.com>
2020-10-19 16:59:04 -0400

    Started Odometry code in ActiveLocation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/RealTimeLocation.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.fieldmapping;
-
-/**
- * The RealTimeLocation uses odometry to find the real-time location of the robot. This,
- * along with a PathFinder, helps create a Field Mapping to allow us to accurately move to specific
- * positions in autonomous.
- */
-public class RealTimeLocation {
-}
