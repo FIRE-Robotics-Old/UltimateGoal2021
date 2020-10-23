a2c536925026d5be6f92288632856e84f8939ceb
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-23 18:20:05 -0400

    Reformats Hardware for Cleanliness
    This cleans up Hardware.java to be cleaner and look nicer.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -19,29 +19,29 @@ public class Hardware {
     //Motors here
 
     //Ecoders
-    public DcMotor frontLeftMotor= null;
+    public DcMotor frontLeftMotor  = null;
     public DcMotor frontRightMotor = null;
-    public DcMotor backLeftMotor = null;
-    public DcMotor backRightMotor = null;
+    public DcMotor backLeftMotor   = null;
+    public DcMotor backRightMotor  = null;
 
     //Servos here
 
-    HardwareMap hwMap = null;
+            HardwareMap hwMap  = null;
     private ElapsedTime period = new ElapsedTime();
     /**
      * Sets up the HardwareMap
      * @param hwMap is the hardware map
      */
 
-    public void init(HardwareMap hwMap){
+    public void init(HardwareMap hwMap) {
         this.hwMap = hwMap;
         //imu set up parameters
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
+        parameters.angleUnit                        = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit                        = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile              = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled                   = true;
+        parameters.loggingTag                       = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
         //Parts in hardware map
@@ -49,28 +49,28 @@ public class Hardware {
         imu.initialize(parameters);
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
 
-        frontLeftMotor = hwMap.get(DcMotor.class, "frontLeftMotor" );
-        frontRightMotor = hwMap.get(DcMotor.class, "frontRightMotor" );
-        backLeftMotor = hwMap.get(DcMotor.class, "backLeftMotor" );
-        backRightMotor = hwMap.get(DcMotor.class, "backRightMotor" );
+        frontLeftMotor  = hwMap.get(DcMotor.class, "frontLeftMotor" );
+        frontRightMotor = hwMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor   = hwMap.get(DcMotor.class, "backLeftMotor"  );
+        backRightMotor  = hwMap.get(DcMotor.class, "backRightMotor" );
 
         //Motor Directions
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection (DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection (DcMotorSimple.Direction.REVERSE);
 
         frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         //Turn off all motors
 
         frontRightMotor.setPower(0);
-        frontLeftMotor.setPower(0);
+        frontLeftMotor.setPower (0);
         backRightMotor.setPower(0);
-        backLeftMotor.setPower(0);
+        backLeftMotor.setPower (0);
     }
 
 }
