92554ce6851bb693d97ca5e61ccb16d56904f426
Marvin4213 <52053423+Marvin4213@users.noreply.github.com>
2020-11-27 16:13:28 -0500

    Update ActiveLocation code
    All functions written, has nullpointer error when run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/ActiveLocation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/ActiveLocation.java
@@ -4,6 +4,9 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.teamcode.Hardware;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
@@ -15,47 +18,131 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
  */
 public class ActiveLocation implements Runnable{ // add implements Runnable{
 
-
+    //Hardware setup
     Hardware robot = new Hardware();
     private BNO055IMU imu;
     private DcMotor frontLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backLeftMotor;
     private DcMotor backRightMotor;
-    private volatile double deltaX;
-    private volatile double deltaY;
-    private volatile double current_Y;
-    private volatile double current_X;
-    private volatile double previous_Y;
-    private volatile double previous_X;
 
+    //var setup
 
+    //change in internal x and y values
+    private volatile double deltaX = 0;
+    private volatile double deltaY = 0;
+    //X and Y based on encoder
+    private volatile double internalCurrentY = 0;
+    private volatile double internalCurrentX = 0;
+    private volatile double internalPreviousY = 0;
+    private volatile double internalPreviousX = 0;
+    //used for sensor values
+    private double yEncoder = 0;
+    private double xEncoder = 0;
+    double angle = 0;
+    //Field location
+    double fieldXPositon;
+    double fieldYPostion;
+    //For stopping the thread
+    private volatile boolean stop = false;
+
+    //Static values for calculations
     final static double tickPerRotation = 8192;
     final static double wheelCircumference = 90 * Math.PI;
-
-    public ActiveLocation(DcMotor frontLeftMotor, DcMotor backRightMotor){
-        this.frontLeftMotor = frontLeftMotor;
-        this.backRightMotor = backRightMotor;
-
-    }
 
     //TODO Write JavaDoc Comment
     /**
      * Figures out distance based on current encoder position over total ticks per rotation and muliplies it by circumfernce
-     * @param ticks
-     * @param encoder
-     * @return
+     * @param ticks tick value
+     * @return mm distance value
      */
-    public static double tickToDistance(double ticks, DcMotor encoder){
-        double currentTicks = encoder.getCurrentPosition();
+    private static double tickToDistance(double ticks){
+        double currentTicks = ticks;
         return ((currentTicks/tickPerRotation) * wheelCircumference);
     }
 
-    public static double DistanceToTicks(double distance, DcMotor encoder){
+    /**
+     * Figures out ticks based on distance over wheel circumference  multiplies it by ticksPerRotation
+     * @param distance mm value
+     * @return returns tick value
+     */
+    private static double DistanceToTicks(double distance){
 
         return ((distance/wheelCircumference)*tickPerRotation);
     }
 
+    /**
+     * Sets the start postion
+     * @param internalCurrentX The current X position mm
+     * @param internalCurrentY The current Y position mm
+     */
+
+    public void setStartPosition(double internalCurrentX, double internalCurrentY){
+        this.internalCurrentY = internalCurrentY;
+        this.internalCurrentX = internalCurrentX;
+    }
+
+    /**
+     * Updates the yEncoder, X encoder, and imu values
+     */
+    private void updateSensors(){
+        yEncoder = frontLeftMotor.getCurrentPosition();
+        xEncoder = backRightMotor.getCurrentPosition();
+        angle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).thirdAngle;
+    }
+
+    /**
+     * Calculates where on the field the robot is located
+     */
+    public void findFieldPosition(){
+        internalPreviousY = internalCurrentY;
+        internalPreviousX = internalCurrentX;
+        synchronized (this) {
+            internalCurrentY = tickToDistance(yEncoder);
+            internalCurrentX = tickToDistance(xEncoder);
+        }
+        deltaY = internalCurrentY - internalPreviousY;
+        deltaX = internalCurrentX - internalPreviousX;
+        fieldXPositon += deltaX * Math.cos(Math.toRadians(angle)) - deltaY * Math.sin(Math.toRadians(angle));
+        fieldYPostion += deltaX * Math.sin(Math.toRadians(angle)) + deltaY * Math.cos(Math.toRadians(angle));
+    }
+
+    /**
+     * Gets Field Y Position
+     * @return Returns the robot's Y position on the field (mm)
+     */
+    public double getFieldYPostion(){
+        updateSensors();
+        findFieldPosition();
+        return fieldYPostion;
+    }
+
+    /**
+     * Gets Field X Position
+     * @return returns X postion on the field (mm)
+     */
+    public double getFieldXPositon(){
+        updateSensors();
+        findFieldPosition();
+        return fieldXPositon;
+    }
+
+    /**
+     * Gets robot angle
+     * @return returns angle
+     */
+    public double getAngle(){
+        return angle;
+    }
+
+    /**
+     * Stops the thread
+     */
+    public void setStop(){
+        stop = true;
+    }
+
+    /**
+     * The function that the thread needs
+     */
     @Override
     public void run() {
 
@@ -64,8 +151,12 @@ public class ActiveLocation implements Runnable{ // add implements Runnable{
         frontLeftMotor = robot.frontLeftMotor;
         backRightMotor = robot.backRightMotor;
 
-        imu = robot.imu;
 
+        imu = robot.imu;
+        while (!stop){
+            updateSensors();
+            findFieldPosition();
+        }
     }
 
 }
