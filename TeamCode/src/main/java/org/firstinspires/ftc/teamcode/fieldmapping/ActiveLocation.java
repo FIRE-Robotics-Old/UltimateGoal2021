e5c50d229d4e4681b7ca8028db6136d6ac89a9a0
Advaith Pillai <advaith.pillai@lps-students.org>
2020-11-27 13:07:43 -0500

    Add Hardware to ActiveLocation
    Set up Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/ActiveLocation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/ActiveLocation.java
@@ -1,17 +1,34 @@
 package org.firstinspires.ftc.teamcode.fieldmapping;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 /**
  * The ActiveLocation uses odometry to find the real-time location of the robot. This,
  * along with a PathFinder, helps create a Field Mapping to allow us to accurately move to specific
  * positions in autonomous.
  */
-public class ActiveLocation { // add implements Runnable{
+public class ActiveLocation implements Runnable{ // add implements Runnable{
+
+
+    Hardware robot = new Hardware();
+    private BNO055IMU imu;
     private DcMotor frontLeftMotor;
     private DcMotor frontRightMotor;
     private DcMotor backLeftMotor;
     private DcMotor backRightMotor;
+    private volatile double deltaX;
+    private volatile double deltaY;
+    private volatile double current_Y;
+    private volatile double current_X;
+    private volatile double previous_Y;
+    private volatile double previous_X;
+
 
     final static double tickPerRotation = 8192;
     final static double wheelCircumference = 90 * Math.PI;
@@ -19,6 +36,7 @@ public class ActiveLocation { // add implements Runnable{
     public ActiveLocation(DcMotor frontLeftMotor, DcMotor backRightMotor){
         this.frontLeftMotor = frontLeftMotor;
         this.backRightMotor = backRightMotor;
+
     }
 
     //TODO Write JavaDoc Comment
@@ -37,4 +55,17 @@ public class ActiveLocation { // add implements Runnable{
 
         return ((distance/wheelCircumference)*tickPerRotation);
     }
+
+    @Override
+    public void run() {
+
+        robot.init(hardwareMap);
+
+        frontLeftMotor = robot.frontLeftMotor;
+        backRightMotor = robot.backRightMotor;
+
+        imu = robot.imu;
+
+    }
+
 }
