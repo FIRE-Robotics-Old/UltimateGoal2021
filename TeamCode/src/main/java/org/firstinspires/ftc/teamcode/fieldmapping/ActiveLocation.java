f1ace7c9ace8f6286d079502bfa2cc696d155e46
Shourya Bansal <shouryabansal2012@gmail.com>
2020-11-27 19:58:53 -0500

    Clean Up ActiveLocation
    Changes the Comments to make them more readable, reformats the variables, and adds necessary basic formatting in the class.
    
    Signed-off-by: Shourya Bansal <shouryabansal2012@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/ActiveLocation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldmapping/ActiveLocation.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.fieldmapping;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -12,151 +11,169 @@ import org.firstinspires.ftc.teamcode.Hardware;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 /**
- * The ActiveLocation uses odometry to find the real-time location of the robot. This,
- * along with a PathFinder, helps create a Field Mapping to allow us to accurately move to specific
- * positions in autonomous.
+ * The ActiveLocation uses odometry to find the real-time location of the robot.
+ * This, along with a PathFinder, helps create a Field Mapping to allow us to
+ * accurately move to specific positions in autonomous.
  */
-public class ActiveLocation implements Runnable{ // add implements Runnable{
+public class ActiveLocation implements Runnable { // add implements Runnable{
 
-    //Hardware setup
+    // Hardware setup
     Hardware robot = new Hardware();
     private BNO055IMU imu;
     private DcMotor frontLeftMotor;
     private DcMotor backRightMotor;
 
-    //var setup
+    //########## VARIABLE SET UP ##########\\
 
-    //change in internal x and y values
-    private volatile double deltaX = 0;
-    private volatile double deltaY = 0;
-    //X and Y based on encoder
+    // X and Y based on encoder
     private volatile double internalCurrentY = 0;
     private volatile double internalCurrentX = 0;
-    private volatile double internalPreviousY = 0;
-    private volatile double internalPreviousX = 0;
-    //used for sensor values
+
+    // Used for sensor values
     private double yEncoder = 0;
     private double xEncoder = 0;
     double angle = 0;
-    //Field location
-    double fieldXPositon;
-    double fieldYPostion;
-    //For stopping the thread
+
+    // Field location
+    double fieldXPosition;
+    double fieldYPosition;
+
+    // For stopping the thread
     private volatile boolean stop = false;
 
-    //Static values for calculations
+    // Static values for calculations
     final static double tickPerRotation = 8192;
     final static double wheelCircumference = 90 * Math.PI;
 
-    //TODO Write JavaDoc Comment
+    // TODO Write JavaDoc Comment
     /**
-     * Figures out distance based on current encoder position over total ticks per rotation and muliplies it by circumfernce
+     * Converts Ticks to Distance
+     *
+     * To calculate the distance, this function calculates the fraction of a total rotation (which
+     * can be greater than 1 rotation) and multiplies it by the circumference, getting the linear
+     * distance from the rotational fraction.
+     *
      * @param ticks tick value
      * @return mm distance value
      */
-    private static double tickToDistance(double ticks){
-        double currentTicks = ticks;
-        return ((currentTicks/tickPerRotation) * wheelCircumference);
+    private static double tickToDistance(double ticks) {
+        return ((ticks / tickPerRotation) * wheelCircumference);
     }
 
     /**
-     * Figures out ticks based on distance over wheel circumference  multiplies it by ticksPerRotation
+     * Converts Distance to Ticks
+     *
+     * To calculate the ticks traveled, the function determines the fraction of a rotation (using
+     * distance and the circumference) and multiplies by the constant ticksPerRotation, returning
+     * the total ticks.
+     *
      * @param distance mm value
      * @return returns tick value
      */
-    private static double DistanceToTicks(double distance){
-
-        return ((distance/wheelCircumference)*tickPerRotation);
+    private static double distanceToTicks(double distance) {
+        return ((distance / wheelCircumference) * tickPerRotation);
     }
 
     /**
-     * Sets the start postion
+     * Sets the start position
+     * 
      * @param internalCurrentX The current X position mm
      * @param internalCurrentY The current Y position mm
      */
 
-    public void setStartPosition(double internalCurrentX, double internalCurrentY){
+    public void setStartPosition(double internalCurrentX, double internalCurrentY) {
         this.internalCurrentY = internalCurrentY;
         this.internalCurrentX = internalCurrentX;
     }
 
     /**
-     * Updates the yEncoder, X encoder, and imu values
+     * Updates Relevant Values for ActiveLocation calculations
+     *
+     * This function updates both the X and Y values of the Encoder's position, as well as the angle
+     * of the robot using the built in IMU on the Rev Hub.
      */
-    private void updateSensors(){
+    private void updateSensors() {
         yEncoder = frontLeftMotor.getCurrentPosition();
         xEncoder = backRightMotor.getCurrentPosition();
         angle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).thirdAngle;
     }
 
     /**
-     * Calculates where on the field the robot is located
+     * Updates the current position of the Robot on the Field
+     *
+     * This function calculates the change in the robot's positional values, recalculating the
+     * position of the robot.
      */
-    public void findFieldPosition(){
-        internalPreviousY = internalCurrentY;
-        internalPreviousX = internalCurrentX;
+    public void findFieldPosition() {
+        double internalPreviousY = internalCurrentY;
+        double internalPreviousX = internalCurrentX;
         synchronized (this) {
             internalCurrentY = tickToDistance(yEncoder);
             internalCurrentX = tickToDistance(xEncoder);
         }
-        deltaY = internalCurrentY - internalPreviousY;
-        deltaX = internalCurrentX - internalPreviousX;
-        fieldXPositon += deltaX * Math.cos(Math.toRadians(angle)) - deltaY * Math.sin(Math.toRadians(angle));
-        fieldYPostion += deltaX * Math.sin(Math.toRadians(angle)) + deltaY * Math.cos(Math.toRadians(angle));
+        double deltaY = internalCurrentY - internalPreviousY;
+        // Change in internal x and y values
+        double deltaX = internalCurrentX - internalPreviousX;
+        fieldXPosition += deltaX * Math.cos(Math.toRadians(angle)) - deltaY * Math.sin(Math.toRadians(angle));
+        fieldYPosition += deltaX * Math.sin(Math.toRadians(angle)) + deltaY * Math.cos(Math.toRadians(angle));
     }
 
     /**
-     * Gets Field Y Position
+     * Gets the Robot's Position on the field on the Y-Axis
+     * 
      * @return Returns the robot's Y position on the field (mm)
      */
-    public double getFieldYPostion(){
+    public double getFieldYPosition() {
         updateSensors();
         findFieldPosition();
-        return fieldYPostion;
+        return fieldYPosition;
     }
 
     /**
-     * Gets Field X Position
+     * Gets the Robot's Position on the field on the X-Axis
+     * 
      * @return returns X postion on the field (mm)
      */
-    public double getFieldXPositon(){
+    public double getFieldXPosition() {
         updateSensors();
         findFieldPosition();
-        return fieldXPositon;
+        return fieldXPosition;
     }
 
     /**
-     * Gets robot angle
+     * Gets the Robot's Angle on the Field, relative to its initial angle
+     * 
      * @return returns angle
      */
-    public double getAngle(){
+    public double getAngle() {
         return angle;
     }
 
     /**
-     * Stops the thread
+     * Sets up the thread to stop
      */
-    public void setStop(){
+    public void setStop() {
         stop = true;
     }
 
     /**
-     * The function that the thread needs
+     * Sets up and starts the thread running in the background
+     *
+     * This function determines what the class will do when set to run on an individual thread,
+     * maintaining the current position of the robot so that autonomous can update and properly
+     * calculate where the robot needs to go and when the robot reached said position.
      */
     @Override
     public void run() {
-
         robot.init(hardwareMap);
 
         frontLeftMotor = robot.frontLeftMotor;
         backRightMotor = robot.backRightMotor;
 
-
         imu = robot.imu;
-        while (!stop){
+        while (!stop) {
             updateSensors();
             findFieldPosition();
         }
     }
-
 }
