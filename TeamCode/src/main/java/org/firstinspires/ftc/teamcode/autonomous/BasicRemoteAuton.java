e3e483ff7331e990fcbbb0c4ef0537c0fe5f6f94
Advaith Pillai <advaith.pillai@lps-students.org>
2020-12-17 13:53:29 -0500

    Add BasicRemoteAuton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasicRemoteAuton.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+import org.firstinspires.ftc.teamcode.fieldmapping.ActiveLocation;
+import org.firstinspires.ftc.teamcode.fieldmapping.PathFinder;
+
+
+@Autonomous(name="BasicRemoteAuton", group="Backup")
+public class BasicRemoteAuton extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    Hardware robot = new Hardware();
+    private DcMotor frontRightMotor;
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+    private BNO055IMU imu;
+    private ActiveLocation AL;
+    private Thread locationThread;
+
+//    private PathFinder PF;
+//    private Thread pathThread;
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        imu = robot.imu;
+
+        frontLeftMotor = robot.frontLeftMotor;
+        frontRightMotor = robot.frontRightMotor;
+        backRightMotor = robot.backRightMotor;
+        backLeftMotor = robot.backLeftMotor;
+
+
+        AL = new ActiveLocation(robot);
+        locationThread = new Thread(AL);
+        locationThread.start();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        try{
+            sleep(3000);
+//comment
+
+            double startTime = runtime.milliseconds();
+            double currentTime = 0;
+
+            AL.setStartPosition(304,0);
+
+            while (AL.getFieldY() < 2100) {
+                frontLeftMotor.setPower(0.3);
+                frontRightMotor.setPower(-0.3);
+                backLeftMotor.setPower(-0.3);
+                backRightMotor.setPower(0.3);
+                currentTime = runtime.milliseconds();
+            }
+
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+
+
+
+
+
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+            telemetry.update();
+
+        }
+        catch (Exception e){
+            telemetry.addData("error:",e.getStackTrace());
+        }
+    }
+}
