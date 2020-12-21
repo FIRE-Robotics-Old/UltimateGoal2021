c9e1f5d659a73e9c933962b5f66aecba80d129f4
Sz77 <63186233+Sz77@users.noreply.github.com>
2020-12-22 01:48:07 +0200

    Add slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/Teleop.java
@@ -26,14 +26,17 @@ public class Teleop extends LinearOpMode {
 
     private ActiveLocation AL;
     private Thread locationThread;
-
+    // TODO change the max speed to 1
+    double maxSpeed =0.5;
     double drive = 0;
     double strafe = 0;
     double twist = 0;
     double intakeAndDeliveryPower = 0;
     double shooterPower =0 ;
     boolean ifReversedIntakePressed = false;
-    boolean shooterIsPressed = true;
+    boolean shooterIsPressed = false;
+    boolean slowModePressed = false;
+    boolean slowMode = false;
     @Override
     public void runOpMode() {
         fL = robot.frontLeftMotor;
@@ -72,31 +75,49 @@ public class Teleop extends LinearOpMode {
             }
             //setting the max speed while keeping the ratio
             // change the number to change the max speed (0-1)
-            // TODO change the max speed to 1
-            if (max > 0.5) {
+
+            if (max > maxSpeed ) {
                 for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
 
             }
+            //slow mode
+            if(gamepad1.a && !slowMode && !slowModePressed)
+            {
+                slowModePressed=true;
+                slowMode= true;
+                maxSpeed = 0.3;
+
+            }
+            else if (gamepad1.a && slowMode &&!slowModePressed)
+            {
+                slowModePressed=true;
+                slowMode= false;
+                maxSpeed = 0.5;
+            }
+            else if(!gamepad1.a)
+            {
+                slowModePressed =false;
+            }
 
                 // reversing the intake and delivery
-            if (gamepad2.back && ifReversedIntakePressed == false)
+            if (gamepad2.back && !ifReversedIntakePressed)
             {
                 intakeAndDeliveryPower *=-1;
                 ifReversedIntakePressed =  true;
             }
-            else
+            else if (!gamepad2.back)
             {
                 ifReversedIntakePressed = false;
             }
 
             //turning on the shooter
 
-            if (gamepad2.a  && shooterIsPressed ==false)
+            if (gamepad2.a  && !shooterIsPressed)
             {
                 shooterIsPressed = true;
                 shooterPower=1;
             }
-            else
+            else if (!gamepad2.a)
             {
                 shooterIsPressed=false;
                 shooterPower=0;
