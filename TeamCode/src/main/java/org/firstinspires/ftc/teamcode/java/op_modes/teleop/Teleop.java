7ead17fc7cd114c24af9b47f94ae7b68a9dad3bb
Shourya Bansal <shouryabansal2012@gmail.com>
2020-12-21 19:04:18 -0500

    Update Formatting in Hardware and Teleop
    This also implements resetAngle in Teleop.java
    
    Signed-off-by: Shourya Bansal <shouryabansal2012@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/Teleop.java
@@ -3,135 +3,129 @@ package org.firstinspires.ftc.teamcode.java.op_modes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.robot.Robot;
 
 import org.firstinspires.ftc.teamcode.java.fieldmapping.ActiveLocation;
 import org.firstinspires.ftc.teamcode.java.utils.Hardware;
 
-
-
-
-@TeleOp
-        (name="teleOp", group="Linear Opmode")
+@TeleOp(name = "Final TeleOp", group = "TeleOp")
 public class Teleop extends LinearOpMode {
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+    public DcMotor intakeDelivery;
+    public DcMotor leftShooter;
+    public DcMotor rightShooter;
+
     Hardware robot;
 
-    public DcMotor fL;
-    public DcMotor fR;
-    public DcMotor bL;
-    public DcMotor bR;
-    public DcMotor iD;
-    public DcMotor lS;
-    public DcMotor rS;
-
-    private ActiveLocation AL;
-    private Thread locationThread;
     // TODO change the max speed to 1
-    double maxSpeed =0.5;
+
+    double maxSpeed = 0.5;
     double drive = 0;
     double strafe = 0;
     double twist = 0;
     double intakeAndDeliveryPower = 0;
-    double shooterPower =0 ;
+    double shooterPower = 0;
+
     boolean ifReversedIntakePressed = false;
     boolean shooterIsPressed = false;
     boolean slowModePressed = false;
     boolean slowMode = false;
+
+    ActiveLocation activeLocation;
+    Thread locationThread;
+
     @Override
     public void runOpMode() {
-        fL = robot.frontLeftMotor;
-        fR = robot.frontRightMotor;
-        bL = robot.backLeftMotor;
-        bL = robot.backRightMotor;
+        frontLeft = robot.frontLeftMotor;
+        frontRight = robot.frontRightMotor;
+        backLeft = robot.backLeftMotor;
+        backLeft = robot.backRightMotor;
 
-        AL = new ActiveLocation(robot);
-        locationThread = new Thread(AL);
+        activeLocation = new ActiveLocation(robot);
+        locationThread = new Thread(activeLocation);
         locationThread.start();
 
-
         waitForStart();
-        //TODO mode change and angel reset  and shooter adjusting
-
+        //TODO mode change and angel reset and shooter adjusting
 
         while (opModeIsActive()) {
             //motors powers calculation
-            drive = gamepad1.left_stick_y *Math.cos(AL.getAngle()) -
-                    gamepad1.left_stick_x * Math.sin(AL.getAngle()) ;
-            strafe =gamepad1.left_stick_x *Math.cos(AL.getAngle()) +
-                    gamepad1.left_stick_y * Math.sin(AL.getAngle()) ;
+            drive = gamepad1.left_stick_y * Math.cos(activeLocation.getAngle()) -
+                    gamepad1.left_stick_x * Math.sin(activeLocation.getAngle());
+            strafe = gamepad1.left_stick_x * Math.cos(activeLocation.getAngle()) +
+                    gamepad1.left_stick_y * Math.sin(activeLocation.getAngle());
             twist = gamepad1.right_stick_x;
             intakeAndDeliveryPower = gamepad2.left_trigger;
+
             // wheel speed calculation
-            double [] speeds = {
+            double[] speeds = {
                     (drive + strafe + twist),
                     (drive - strafe - twist),
                     (drive - strafe + twist),
                     (drive + strafe - twist)
             };
+
             //finding the biggest drive motor power
-            double max = Math.abs((speeds [0]));
+            double max = Math.abs((speeds[0]));
             for (double speed : speeds) {
                 if (max < Math.abs(speed)) max = Math.abs(speed);
             }
+
             //setting the max speed while keeping the ratio
             // change the number to change the max speed (0-1)
 
-            if (max > maxSpeed ) {
+            //TODO: This turns everything scaling speed to 1, not max speed. Quick Fix by Multiplying by max speed
+            if (max > maxSpeed) {
                 for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
-
             }
-            //slow mode
-            if(gamepad1.a && !slowMode && !slowModePressed)
-            {
-                slowModePressed=true;
-                slowMode= true;
+
+            // slow mode
+            if (gamepad1.a && !slowMode && !slowModePressed) {
+                slowModePressed = true;
+                slowMode = true;
                 maxSpeed = 0.3;
-
-            }
-            else if (gamepad1.a && slowMode &&!slowModePressed)
-            {
-                slowModePressed=true;
-                slowMode= false;
+            } else if (gamepad1.a && slowMode && !slowModePressed) {
+                slowModePressed = true;
+                slowMode = false;
                 maxSpeed = 0.5;
-            }
-            else if(!gamepad1.a)
-            {
-                slowModePressed =false;
+            } else if (!gamepad1.a) {
+                slowModePressed = false;
             }
 
-                // reversing the intake and delivery
-            if (gamepad2.back && !ifReversedIntakePressed)
-            {
-                intakeAndDeliveryPower *=-1;
-                ifReversedIntakePressed =  true;
+            // Angle Resetting
+            if (gamepad1.start) {
+                activeLocation.resetAngle();
             }
-            else if (!gamepad2.back)
-            {
+
+            // reversing the intake and delivery
+            if (gamepad2.back && !ifReversedIntakePressed) {
+                intakeAndDeliveryPower *= -1;
+                ifReversedIntakePressed = true;
+            } else if (!gamepad2.back) {
                 ifReversedIntakePressed = false;
             }
 
             //turning on the shooter
 
-            if (gamepad2.a  && !shooterIsPressed)
-            {
+            if (gamepad2.a && !shooterIsPressed) {
                 shooterIsPressed = true;
-                shooterPower=1;
+                shooterPower = 1;
+            } else if (!gamepad2.a) {
+                shooterIsPressed = false;
+                shooterPower = 0;
             }
-            else if (!gamepad2.a)
-            {
-                shooterIsPressed=false;
-                shooterPower=0;
-            }
+
             //setting the speed to the motors
-            fL.setPower(speeds[0]);
-            fR.setPower(speeds[1]);
-            bL.setPower(speeds[2]);
-            bR.setPower(speeds[3]);
-            iD.setPower(intakeAndDeliveryPower);
-            lS.setPower(shooterPower);
-            rS.setPower(shooterPower);
-
-
+            frontLeft.setPower(speeds[0]);
+            frontRight.setPower(speeds[1]);
+            backLeft.setPower(speeds[2]);
+            backRight.setPower(speeds[3]);
+            intakeDelivery.setPower(intakeAndDeliveryPower);
+            leftShooter.setPower(shooterPower);
+            rightShooter.setPower(shooterPower);
         }
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/utils/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/utils/Hardware.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.java.utils;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -14,68 +15,66 @@ import java.util.Locale;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
-public class Hardware{
+public class Hardware {
 
     public BNO055IMU imu;
 
-    public DcMotor frontLeftMotor =null;
-    public DcMotor frontRightMotor = null;
-    public DcMotor backLeftMotor = null;
-    public DcMotor backRightMotor = null;
+    public DcMotorEx frontLeftMotor = null;
+    public DcMotorEx frontRightMotor = null;
+    public DcMotorEx backLeftMotor = null;
+    public DcMotorEx backRightMotor = null;
+
     public DcMotor rightShooter = null;
-    public DcMotor leftShooter = null ;
+    public DcMotor leftShooter = null;
     public DcMotor intakeAndDelivery = null;
 
-
-
-
-    HardwareMap hwMap = null;
+    HardwareMap hardwareMap = null;
     private ElapsedTime period = new ElapsedTime();
 
     /**
      * Sets up the HardwareMap
-     * @param hwMap is the hardware map
+     *
+     * @param hardwareMap is the hardware map
      */
-    public void init(HardwareMap hwMap) {
-        this.hwMap = hwMap;
-        //imu set up parameters
+    public void init(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+        // imu set up parameters
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit                        = BNO055IMU.AngleUnit.RADIANS;
-        parameters.accelUnit                        = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile              = "BNO055IMUCalibration.json";
-        parameters.loggingEnabled                   = true;
-        parameters.loggingTag                       = "IMU";
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-        //Parts in hardware map
-        imu = hwMap.get(BNO055IMU.class, "imu");
+        // Parts in hardware map
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
 
-        frontLeftMotor  = hwMap.get(DcMotor.class, "frontLeftMotor" );
-        frontRightMotor = hwMap.get(DcMotor.class, "frontRightMotor");
-        backLeftMotor   = hwMap.get(DcMotor.class, "backLeftMotor"  );
-        backRightMotor  = hwMap.get(DcMotor.class, "backRightMotor" );
+        frontLeftMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontRightMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backRightMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "backRightMotor");
 
-        intakeAndDelivery  = hwMap.get(DcMotor.class, "intakeAndDelivery" );
-        rightShooter = hwMap.get(DcMotor.class, "rightShooter");
-        leftShooter = hwMap.get(DcMotor.class, "leftShooter");
+        intakeAndDelivery = hardwareMap.get(DcMotor.class, "intakeAndDelivery");
+        rightShooter = hardwareMap.get(DcMotor.class, "rightShooter");
+        leftShooter = hardwareMap.get(DcMotor.class, "leftShooter");
 
-
-        //Motor Directions
+        // Motor Directions
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-        frontLeftMotor.setDirection (DcMotor.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
-        backLeftMotor.setDirection (DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
         intakeAndDelivery.setDirection(DcMotor.Direction.FORWARD);
         rightShooter.setDirection(DcMotor.Direction.FORWARD);
         leftShooter.setDirection(DcMotor.Direction.REVERSE);
 
         frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontLeftMotor.setMode (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeftMotor.setMode (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -83,18 +82,20 @@ public class Hardware{
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeftMotor.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeftMotor.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         rightShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         leftShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         intakeAndDelivery.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-        //Turn off all motors
+        // Turn off all motors
         frontRightMotor.setPower(0);
-        frontLeftMotor.setPower (0);
+        frontLeftMotor.setPower(0);
         backRightMotor.setPower(0);
-        backLeftMotor.setPower (0);
+        backLeftMotor.setPower(0);
+
         intakeAndDelivery.setPower(0);
         rightShooter.setPower(0);
         leftShooter.setPower(0);
