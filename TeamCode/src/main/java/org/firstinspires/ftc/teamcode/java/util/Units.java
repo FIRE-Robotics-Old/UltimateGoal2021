b4466c3b5979fd9f66f1ebe2811bbde8d730be37
Shourya Bansal <shouryabansal2012@gmail.com>
2021-01-20 16:31:52 -0500

    Start creating constants and unit converters

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/Units.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.java.util;
+
+public enum Units {
+    NANOMETER   (Constants.nanometerToMillimeter),
+    MICROMETER  (Constants.micrometerToMillimeter),
+    MILLIMETERS (Constants.millimeterToMillimeter),
+    CENTIMETERS (Constants.centimeterToMillimeter),
+    DECIMETERS  (Constants.decimeterToMillimeter),
+    METERS      (Constants.meterToMillimeter),
+    DEKAMETERS  (Constants.dekameterToMillimeter),
+    HECTOMETER  (Constants.hectometerToMillimeter),
+    KILOMETERS  (Constants.kilometerToMillimeter),
+    INCHES      (Constants.inchToMillimeter),
+    FEET        (Constants.feetToMillimeter),
+    YARDS       (Constants.yardToMillimeter),
+    MILES       (Constants.mileToMillimeter);
+
+    private final double multiplier;
+
+    Units(double multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public double getMultiplier() {
+        return this.multiplier;
+    }
+}
