8ad4189d0d0bbb755bdff385ce8d328bc6ee847e
Marvin4213 <52053423+Marvin4213@users.noreply.github.com>
2021-02-08 00:58:12 -0500

    Add color key combination loop to test values for cv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/tests/CameraVisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/tests/CameraVisionTest.java
@@ -3,7 +3,12 @@ package org.firstinspires.ftc.teamcode.java.tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.java.vision.HeightDetector;
+//import org.firstinspires.ftc.teamcode.java.vision.HeightDetector;
+import org.firstinspires.ftc.teamcode.java.vision.RingHeightPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
 
 @TeleOp(name = "Camera Vision Test", group = "Testing")
 public class CameraVisionTest extends LinearOpMode {
@@ -12,10 +17,41 @@ public class CameraVisionTest extends LinearOpMode {
 		HeightDetector heightDetector = new HeightDetector(hardwareMap);
 		waitForStart();
 
-        while (opModeIsActive()) {
-            telemetry.addData("Height Position", heightDetector.getHeight());
-            telemetry.update();
-            sleep(50);
-        }
+		RingHeightPipeline pipeline = new RingHeightPipeline(telemetry);
+
+	    OpenCvInternalCamera camera;
+	    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+			    "cameraMonitorViewId",
+			    "id",
+			    hardwareMap.appContext.getPackageName()
+	    );
+	    camera = OpenCvCameraFactory.getInstance().createInternalCamera(
+			    OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId
+	    );
+
+	    camera.setPipeline(pipeline);
+
+	    camera.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+
+	    camera.openCameraDeviceAsync(() ->
+			    camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT)
+	    );
+
+	    waitForStart();
+
+	    while (opModeIsActive()) {
+
+	    }
+
+	    camera.stopStreaming();
+//
+//        HeightDetector heightDetector = new HeightDetector(hardwareMap);
+//        waitForStart();
+//
+//        while (opModeIsActive()) {
+//            telemetry.addData("Height Position", heightDetector.getHeight());
+//            telemetry.update();
+//            sleep(50);
+//        }
     }
 }
