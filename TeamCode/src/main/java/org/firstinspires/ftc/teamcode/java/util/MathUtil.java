054c1d01a0f7e30205a5a53686df12a17411dbc2
Shourya Bansal <shouryabansal2012@gmail.com>
2021-02-23 09:58:07 -0500

    Optimize MathUtil.pow to use binary exponentiation
    Now, instead of running in time proportional to "times", it is running in time proportional to log_2("times"), giving huge gains, especially for larger numbers.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/MathUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/MathUtil.java
@@ -50,44 +50,72 @@ public class MathUtil {
 	}
 
 	public static double pow(double num, int times) {
-		double sol = num;
-		while (--times != 0) sol *= num;
+		double sol = 1D;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 
 	public static float pow(float num, int times) {
-		float sol = num;
-		while (--times != 0) sol *= num;
+		float sol = 1F;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 
 	public static int pow(byte num, int times) {
-		int sol = num;
-		while (--times != 0) sol *= num;
+		int sol = 1;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 
 	public static int pow(short num, int times) {
-		int sol = num;
-		while (--times != 0) sol *= num;
+		int sol = 1;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 
 	public static long pow(int num, int times) {
-		long sol = num;
-		while (--times != 0) sol *= num;
+		long sol = 1L;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 
 	public static int powInt(int num, int times) {
-		int sol = num;
-		while (--times != 0) sol *= num;
+		int sol = 1;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 
 	public static long pow(long num, long times) {
-		long sol = num;
-		while (--times != 0) sol *= num;
+		long sol = 1;
+		while (times > 0) {
+			if ((times & 1) == 1) sol *= num;
+			num *= num;
+			times >>= 1;
+		}
 		return sol;
 	}
 }
