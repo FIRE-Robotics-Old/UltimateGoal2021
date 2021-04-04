9b500a969f6e3407440e47d205a9f35ee81067f3
Marvin4213 <52053423+Marvin4213@users.noreply.github.com>
2021-04-04 17:10:44 -0400

    Update PidfConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/PidfConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/PidfConstants.java
@@ -4,12 +4,12 @@ public final class PidfConstants {
 	//TODO get rid of this in libary
 		//US
 	public static final PidfController USDrive = new PidfController(0, 0, 0, 0);
-	//public static final PidfController USDrive = new PidfController(0.005, 0, 0.0/*2605/*125*/, 0);
+	//public static final PidfController USDrive = new PidfController(0.000562, 0, 0.0/*2605/*125*/, 0);
 	public static final PidfController USStrafe = new PidfController(0, 0, 0, 0);
 	//public static final PIDFController USStrafe = new PIDFController(0.00125, 0, 0, 0);
 	//public static final PidfController USTurn = new PidfController(0, 0, 0, 0);
-	public static final PidfController USTurn = new PidfController(0.119375, 0, 0./*3695*/, 0); //.35
-	//turn is between 0.1175 and 0.12
+	public static final PidfController USTurn = new PidfController(0.27, 0, 0./*3695*/, 0); //.35
+	//turn decent: 0.27
 		//Israel
 	public static final PidfController ISDrive = new PidfController(0,0,0,0);
 	public static final PidfController ISStrafe = new PidfController(0,0,0,0);
